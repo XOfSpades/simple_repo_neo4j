cc77cfa0f1ff76a1df0921f52d3a8005cfa25208
Bernhard Stoecker <bernhard.stoecker@recogizer.de>
2017-11-13 10:27:45 +0100

    Start bolt_sips

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule SimpleRepoNeo4j.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :bolt_sips]]
   end
 
   defp elixirc_paths(:prod), do: ["lib"]
